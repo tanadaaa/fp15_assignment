f3829a1e0c9f0a6abac457a72449f1d5afa200c0
ryota1637 <ryou-g@ksh.biglobe.ne.jp>
2023-02-13 16:00:39 +0900

    HPゲージ系修正

--- anim.c
+++ anim.c
@@ -8,11 +8,11 @@ struct color black = { 0, 0, 0 };
 void flash(int n){
     for(i = 0; i < n; i++){
         img_clear();
-        if(i >= 0 && i < n/5){img_fillrect(black, 0, 0, 639, 479);}
+        if(i >= 0 && i < n/5){img_fillrect(black, 0, 479, 639, 0);}
         if(i >= n/5 && i < n*2/5){}
-        if(i >= n*2/5 && i < n*3/5){img_fillrect(black, 0, 0, 639, 479);}
+        if(i >= n*2/5 && i < n*3/5){img_fillrect(black, 0, 479, 639, 0);}
         if(i >= n*3/5 && i < n*4/5){}
-        if(i >= n*4/5){img_fillrect(black, 0, 0, 639, 479);}
+        if(i >= n*4/5){img_fillrect(black, 0, 479, 639, 0);}
         img_write();
     }
     
@@ -20,6 +20,6 @@ void flash(int n){
 
 void f_mov_framy(int i){
     img_clear();
-    img_framy(black, 680-i*7, 200);
+    img_framy(black, 680-i*7, 400);
     img_write();
 }
--- animate_test.c
+++ animate_test.c
@@ -6,13 +6,22 @@ int main(void) {
   struct color white = { 255, 255, 255};
   flash(5);
   int i;
-  for(i = 0;i < 80; i++){
+  for(i = 0;i < 40; i++){
     f_mov_framy(i);
+  }
+  for(i = 0; i < 35; i++) {
+    img_clear();
+    img_framy(black, 400, 400);
+    img_hp_framy(8*i-250, 400, 100);
+    img_write();
   }
   for(i = 0;i < 80;i++){
     img_clear();
+    img_framy(black, 400, 400);
+    img_hp_framy(30, 400, 100);
     framy_c(100, 100);
     mickey_c(100,50);
+    img_hp_mickey(600, 200, 100);
     img_write();
   }
 }
